26d133a81c1a39c5049e26c344b0d3b6f4e38c97
Adam Short <adamshort1994@gmail.com>
2015-11-16 12:47:54 +0000

    First loop repeats shape

--- MyAnimation.hs
+++ MyAnimation.hs
@@ -4,24 +4,24 @@ import Animation
 
 picture :: Animation
 picture = 
-    (translate 
-        -- translate param 1
-        (cycleSmooth 2 [(0, 0), (100, 100), (200, 200), (300, 300), (400, 400)])
-        -- translate param 2
-        (
-            ( scale (repeatSmooth (1, 1) [(0, (1, 1)), (10, (2, 2))])
-                ( withGenPaint (always blue) (repeatSmooth 1 [(0, 1), (2, 0.5), (4, 0), (10, 0)])
-                    (circle (always 75)) 
-                    `plus` 
-                    (translate 
-                        (always (-25, -25)) (
-                            (withPaint (always white) (circle (always 100)))
+    (combine [
+        (translate 
+            (cycleSmooth (2 + i) [(0, 0), (100, 100), (200, 200), (300, 300), (400, 400)])
+            (
+                ( scale (repeatSmooth (1, 1) [(0, (1, 1)), (10, (2, 2))])
+                        ( withGenPaint (always blue) (repeatSmooth 1 [(0, 1), ((2 + i), 0.5), ((4 + i), 0), ((10 + i), 0)])
+                            (circle (always 75)) 
+                            `plus` 
+                            (translate 
+                                (always (-25, -25)) (
+                                    (withPaint (always white) (circle (always 100)))
+                                )
+                            )
                         )
-                    )
                 )
             )
         )
-    )
+    | i <- [1..5]])
 
 test :: IO ()
 test = writeFile "test.svg" (svg 800 600 picture)
