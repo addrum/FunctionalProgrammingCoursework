0452d70ba3a05ff94a9578c978f3640b13704c08
Adam Short <adamshort1994@gmail.com>
2015-11-19 09:15:37 +0000

    Change shape to polygon

--- MyAnimation.hs
+++ MyAnimation.hs
@@ -10,25 +10,12 @@ picture =
             (
                 ( scale (repeatSmooth (1, 1) [((0 + i), (1, 1)), ((10 + i), (2, 2))])
                         ( withGenPaint (always blue) (repeatSmooth 1 [(0, 1), ((2 + i), 0.5), ((4 + i), 0), ((10 + i), 0)])
-                            (circle (always 75)) 
-                            `plus` 
-                            (translate 
-                                (always (-25, -25)) (
-                                    (withBorder (always red) (always 1)
-                                        (withPaint (always white) (circle (always 100)))
-                                    )
-                                )
-                            )
+                            (polygon [(100, 0), (70, 70), (0, 100), (60, 60)])
                         )
                 )
             )
         )
     | i <- [1..5]])
 
-poly = 
-    polygon [(10, 0), (10, 10), (0, 10)]
-
-
 test :: IO ()
 test = writeFile "test.svg" (svg 800 600 picture)
-p = writeFile "poly.svg" (svg 800 600 poly)
