dec12c8b9a7fef24ee715871f829023251f5f0b3
Adam Short <adamshort1994@gmail.com>
2015-11-26 11:38:44 +0000

    Add helper function for positioning cart

--- MyAnimation.hs
+++ MyAnimation.hs
@@ -2,8 +2,11 @@ module MyAnimation where
 
 import Animation
 
--- x, y, cartSpeed, radius, startX, startY, endX, endY, angle, wheelSpeed
-type Cart = (Double, Double, Double, Double, Double, Double, Double, Double, Double, Double)
+-- position cartSpeed, radius, startPoint, endPoint, angle, wheelSpeed
+type Cart = (Point, Double, Double, Point, Point, Double, Double)
+
+picture :: Animation
+picture = allCarts cartProperties
 
 wheelBody :: Double -> Animation
 wheelBody radius = 
@@ -21,43 +24,50 @@ spoke radius =
             )
         | i <- [1..2]]
 
-wheel :: Double -> Double -> Double -> Double -> Animation
-wheel x y speed radius =
-        translate (always (x, y))
+wheel :: Point -> Double -> Double -> Animation
+wheel position speed radius =
+        translate (always position)
             (rotate (spinner speed)
             (wheelBody radius `plus` spoke radius))
 
-cart :: Double -> Double -> Animation
-cart x y =
+cart :: Point -> Animation
+cart (x, y) =
         withPaint (always black)
             (polygon [((x - 40), 0), (((x * 3) + 40), 0), ((x * 3), y), (x, y)])
 
 minecart :: Cart -> Animation
-minecart (x, y, cartSpeed, radius, startX, startY, endX, endY, angle, wheelSpeed) =
-            translate (repeatSmooth (startX, startY) [(0, (startX, startY)), (cartSpeed, (endX, endY))])
-            (rotate (always angle)
-                (
-                    (cart x y) 
-                    `plus` 
-                    ((wheel x y wheelSpeed radius) 
-                    `plus` 
-                    (wheel (x * 3) y wheelSpeed radius))
+minecart (position, cartSpeed, radius, startPoint, endPoint, angle, wheelSpeed) =
+            moveCart (cartSpeed, startPoint, endPoint)
+                (rotate (always angle)
+                    (
+                        (cart position) 
+                        `plus` 
+                        ((wheel position (speed wheelSpeed) radius) 
+                        `plus` 
+                        (wheel (secondWheel position) (speed wheelSpeed) radius))
+                    )
                 )
-            )
+
+secondWheel :: Point -> Point
+secondWheel (x, y) = ((x * 3), y)
+
+speed :: Double -> Double
+speed radius = 50 / radius
+
+moveCart :: (Double, Point, Point) -> Animation -> Animation
+moveCart (cartSpeed, startPoint, endPoint) =
+        translate (repeatSmooth startPoint [(0, startPoint), (cartSpeed, endPoint)])
 
 cartProperties :: [Cart]
 cartProperties = 
                 [
-                    (60, 70, 10, 30, -300, 300, 1000, 400, 0, 10),
-                    (30, 40, 20, 15, -400, 500, 1000, 100, -24, 20),
-                    (40, 50, 15, 15, 1000, -100, -300, 200, -15, -15)
+                    ((60, 70), 10, 30, (-300, 300), (1000, 400), 0, 10),
+                    ((30, 40), 20, 15, (-400, 500), (1000, 100), -24, 29),
+                    ((40, 50), 15, 15, (1000, -100), (-300, 200), -15, -15)
                 ]
 
 allCarts :: [Cart] -> Animation 
 allCarts carts = combine (map minecart carts)
-
-picture :: Animation
-picture = allCarts cartProperties
 
 test :: IO ()
 test = writeFile "test.svg" (svg 800 600 picture)
